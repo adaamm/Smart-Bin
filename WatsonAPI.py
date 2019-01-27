d7a61712ce088f22f56de3907eb6fd73aa54ee76
Adam Diallo <adam_diallo@live.fr>
2019-01-27 01:28:33 -0500

    Add files via upload

---
+++ WatsonAPI.py
@@ -0,0 +1,7 @@
+from watson_developer_cloud import VisualRecognitionV3
+
+visual_recognition = VisualRecognitionV3(
+    version='{2018-03-19}', #date of last version
+    iam_apikey='9lBEj-dieX1hFWNirois_Zcr5dIq1dvD6HKv8slcit0h'
+)
+
---
+++ WatsonApiUse.py
@@ -0,0 +1,29 @@
+import json
+import pprint
+from watson_developer_cloud import VisualRecognitionV3
+
+visual_recognition = VisualRecognitionV3(
+    '2018-03-19',
+    iam_apikey='9lBEj-dieX1hFWNirois_Zcr5dIq1dvD6HKv8slcit0h') #API Key on IBM Watson
+
+with open('./image.jpg', 'rb') as images_file:
+    classes = visual_recognition.classify(
+        images_file,
+        threshold='0.6',
+	classifier_ids='DefaultCustomModel_117701744').get_result()
+#print(json.dumps(classes, indent=2))
+
+#word to look for
+words = ("trash","metal", "plastic", "paper", "cardboard")
+with open('waste.txt', 'w') as outfile:
+ json.dump(classes, outfile)
+
+with open("waste.txt") as openfile:
+    for line in openfile:
+        for part in line.split():
+            for m in words:
+                if m in part:
+                  result = m
+                  print(part)
+                 #just to test what is inside of resut print(result)
+
