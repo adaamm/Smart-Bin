516e224ce5e2b57f7ac6e7451c0c5d7578e086cc
Adam Diallo <adam_diallo@live.fr>
2019-01-27 01:49:29 -0500

    Update WatsonApiUse.py

--- WatsonApiUse.py
+++ WatsonApiUse.py
@@ -4,7 +4,7 @@ from watson_developer_cloud import VisualRecognitionV3
 
 visual_recognition = VisualRecognitionV3(
     '2018-03-19',
-    iam_apikey='9lBEj-dieX1hFWNirois_Zcr5dIq1dvD6HKv8slcit0h') #API Key on IBM Watson
+    iam_apikey='enterapihere') #API Key on IBM Watson
 
 with open('./image.jpg', 'rb') as images_file:
     classes = visual_recognition.classify(
