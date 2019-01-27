5954868782916f370771a6e5a7d866799d3dc4dd
Adam Diallo <adam_diallo@live.fr>
2019-01-27 04:16:57 -0500

    Update Trigger.py

--- Trigger.py
+++ Trigger.py
@@ -1,14 +1,4 @@
-import numpy as np
-import cv2
 
-cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop)
-ret,frame = cap.read() # return a single frame in variable `frame`
+import os
 
-while(True):
-    cv2.imshow('image',frame) #display the captured image
-    if cv2.waitKey(1) & 0xFF == ord('y'): #save on pressing 'y'
-        cv2.imwrite('image.png',frame)
-        cv2.destroyAllWindows()
-        break
-
-cap.release()
+os.system("fswebcam image.jpg")
